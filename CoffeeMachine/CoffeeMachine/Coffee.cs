8cd4afca79a98cfa76039adbd4e94f8f2d57d3c6
Keller Arthur <akeller@gemsdev.com>
2019-03-03 15:38:00 +0600

    +CoffeeMAchineProject

---
+++ CoffeeMachine/CoffeeMachine/Coffee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeMachine
+{
+	class Coffee
+	{
+		private string _coffeeSelection;
+		private int _quantity;
+
+		public int GetQuantity();
+		public  GetSelection();
+	}
+}
